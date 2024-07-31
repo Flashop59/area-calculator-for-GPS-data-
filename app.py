bc6474cb7561ce34358e813be0e2e60ce82f986b
Flashop59 <129614252+Flashop59@users.noreply.github.com>
2024-07-31 09:52:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ st.markdown("""
         .header img { height: 60px; margin-right: 20px; }
     </style>
     <div class="header">
-        <img src="https://ibb.co/XxfRB54" alt="Logo">
+        <img src="https://i.ibb.co/JjWJLpd/image.png" alt="Logo">
         <h1>Field Area and Time Calculation from GPS Data</h1>
     </div>
 """, unsafe_allow_html=True)
